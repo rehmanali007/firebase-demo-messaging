d3093ea769a42ef3ee9c931ff98120aba9880168
Rehman Ali <rehmanali.9442289@gmail.com>
2020-11-04 20:11:25 +0500

    Demo app demonstrating the firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase'
 
 function App() {
+  const messaging = firebase.messaging()
+  Notification.requestPermission().then((permission) => {
+    messaging.getToken().then((token) => {
+      console.log(token)
+    }).catch((err) => {
+      console.log('Error!')
+      console.log(err)
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h3>Hello, World!</h3>
     </div>
   );
 }
